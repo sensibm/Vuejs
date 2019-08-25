7343d724ce3503b31ce14b938e56bbd621363a0d
sensibm <sens.sg@gmail.com>
2019-08-25 19:27:17 +0800

    assets and components

---
+++ components/ProductListOne.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="Product_List_One_F">
+      <h2> Product List One </h2>
+      <ul>
+          <li v-for="product in prod">
+            <span class='name'>  @ {{ product.name }}</span>
+            <span class='price'> ${{ product.price }}</span>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+    props: ['prod']
+}
+</script>
+
+<style>
+
+</style>
